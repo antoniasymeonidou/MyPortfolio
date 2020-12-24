e73dd6dc884963f4e41727dee7eed3149e169736
Antonia Symeonidou <antonia.symeonidou@outlook.com>
2020-12-24 15:51:15 +0000

    color mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-website';
+  
+  dark_mode: boolean = false;
+  color_mode = "light"
+
+  ngInit() {
+    this.dark_mode = window.localStorage.getItem("dark_mode") == "true"
+    this.setColourMode()
+  }
+
+  toggle_dark_mode() {
+    this.dark_mode = !this.dark_mode;
+    window.localStorage.setItem("dark_mode", JSON.stringify(this.dark_mode))
+    this.setColourMode()
+  }
+
+  
+
+  private setColourMode() {
+    if (this.dark_mode)
+      this.color_mode = "dark"
+    else
+      this.color_mode = "light"
+  }
 }
